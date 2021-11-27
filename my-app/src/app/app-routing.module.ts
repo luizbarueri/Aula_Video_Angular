694f40db75176779257d13988dfd1994a9e670f2
luizbaruer <luizbarueri@gmail.com>
2021-11-27 15:20:25 -0300

    adicionado menu e cabeçalho

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { ServicosComponent } from './servicos/servicos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'servicos',
+    component: ServicosComponent
+  }
+  ,
+  {
+    path: 'contato',
+    component: ContatoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
